8f88eb9e6a52b601551bdd8efd1e503dbfb5485a
Valuablehope <101519033+Valuablehope@users.noreply.github.com>
2025-09-10 00:19:32 +0300

    Beta II

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -21,8 +21,7 @@ import {
   FaHeart,
   FaArrowUp
 } from 'react-icons/fa'
-// import logo from '../../assets/logo_rse.png'
-const logo = '/logo_rse.png'
+import logo from '../../assets/logo_rse.png'
 import './Footer.css'
 
 const Footer = () => {
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,8 +9,7 @@ import {
   FaGraduationCap,
   FaStar
 } from 'react-icons/fa'
-// import logo from '../../assets/logo_rse.png'
-const logo = '/logo_rse.png'
+import logo from '../../assets/logo_rse.png'
 import './Navbar.css'
 
 const Navbar = () => {
